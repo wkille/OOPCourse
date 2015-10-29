f379ae4a99fa341b922ccc0d844b6cdf766009a5
wkille <wayne_kille@hotmail.com>
2015-10-29 16:49:59 +0000

    constructor & destructer classes updated

--- index.php
+++ index.php
@@ -4,18 +4,38 @@ class Baddie
 {
     public $evilness = 10;
     
-    public function __construct() {
+    public $name;
+    
+    // __construct is a 'magic method' that happens everytime an object is created
+    public function __construct($evilness, $name) {
         
-        echo "A ".__CLASS__." has been created!";
+        $this->evilness = $evilness;
+        
+        $this->name = $name;
+        
     }
+    
+    // __destruct is a 'magic method' that happens everytime an object is destroyed
+    public function __destruct() {
+        
+        echo "A ".__CLASS__." has been destroyed!";
+        
+    }
+
     
 }
 
-$ganon = new Baddie;
+$ganon = new Baddie($evilness=8, $name="Ganon");
 
-$scaldera = new Baddie;
+//$scaldera = new Baddie($evilness=5, $name="Scaldera");
+//$scaldera = new Baddie(5, "Scaldera");
+
+
+unset($ganon);
 
 //var_dump($ganon);
+
+//var_dump($scaldera);
 
 
 ?>
