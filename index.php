c51dd725a187904de3c3b9306a4c1a2e8d47d338
wkille <wayne_kille@hotmail.com>
2015-10-29 16:14:16 +0000

    created two instances of Baddie & two methods to change evilness property

--- index.php
+++ index.php
@@ -2,19 +2,35 @@
 
 class Baddie
 {
-	public $evilness = 10;
-	
-	public function makeMoreEvil() {
-	    
-	    $this->evilness = 20;
-	    return $this->evilness;
-	}
-	
+    public $evilness = 10;
+    
+    public function evilUpTo15() {
+        
+        $this-> evilness = 15;
+    }
+    
+    public function evilUpTo20() {
+        
+        $this-> evilness = 20;
+    }
+    
+    
 }
 
-$ganon = new Baddie;
+$hugrot = new Baddie;
+    
+$shaboo = new Baddie;
 
-echo $ganon -> makeMoreEvil();
+$hugrot-> evilUpTo15();
+
+$shaboo-> evilUpTo20();
+
+var_dump($hugrot);
+
+var_dump($shaboo);
+
+
+
 
 
 ?>
