b46f45c490a8f6d570d624ea030857f2aff3efbb
wkille <wayne_kille@hotmail.com>
2015-11-02 14:26:49 +0000

    overwriting methods and private and protected

--- index.php
+++ index.php
@@ -1,63 +1,36 @@
 <?php
 
-/* class HigherOrLower
-{
-    public function __construct() {
+class Baddie {
+    
+    private $evilness = 10;
+    
+    public function getEvilness() {
         
-        for($i=0; $i<10; $i++) {
-            
-            $turn = rand(0, 10);
-            echo"$turn<br />";
-            ob_flush();
-            flush();
-            sleep(2);
-        }
+        return $this->evilness;
     }
     
-    //if input is up arrow && $i<$i+1 then $i+1 is green
-    //else if input is up arrow && $i>$i+1 then $i+1 is red
-    //else if input is down arrow && $i<$i+1 then $i+1 is red
-    //else if input is down arrow && $i>$i+1 then $i+1 is green
-    //else $i+1 is red
-    
-
-    
-}
-
-$game = new HigherOrLower();
-
-*/
-
-class Number {
-    
-    public $number;
-    
-    public function __construct() {
+    public function setEvilness($evilness) {
         
-        $this->number = rand(1,10);
-        
-        echo $this->number;
-        
-        echo "<br />";
-        
-        ob_flush();
-        
-        flush();
-        
-        sleep(1);
-        
-        unset($this);
-        
+        $this->evilness = $evilness;
     }
     
+    /*
     public function __destruct() {
         
-        $number = new Number;
+        echo "You beat the baddie!";
+    }
+    */
+    
+}
+
+class Boss extends Baddie {
+    
+    public function getBaddieEvilness() {
         
-        
+        return $this->evilness;
     }
 }
 
-$number = new Number;
+$ganon = new Boss;
 
-?>
+echo $ganon->getEvilness();
